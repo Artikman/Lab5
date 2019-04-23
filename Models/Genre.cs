d5b7a6838e62774b236cc9eb2b93cf5ab9855aa8
Artikman <47893341+Artikman@users.noreply.github.com>
2019-04-23 13:01:20 +0300

    Add files via upload

---
+++ Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_4.Models
+{
+    public class Genre
+    {
+        [Required]
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Film> Films { get; set; }
+        public Genre()
+        {
+            Films = new List<Film>();
+        }
+    }
+}
